6a3a6da1f07025435e7d748d0dd069c65173b743
Beto Dealmeida <roberto@dealmeida.net>
2026-04-17 23:25:25 -0400

    Fix edit dataset

--- superset-frontend/src/features/semanticViews/SemanticViewEditModal.tsx
+++ superset-frontend/src/features/semanticViews/SemanticViewEditModal.tsx
@@ -36,8 +36,8 @@ interface SemanticViewEditModalProps {
   show: boolean;
   onHide: () => void;
   onSave: () => void;
-  addDangerToast: (msg: string) => void;
-  addSuccessToast: (msg: string) => void;
+  addDangerToast?: (msg: string) => void;
+  addSuccessToast?: (msg: string) => void;
   semanticView: {
     id: number;
     table_name: string;
@@ -50,8 +50,8 @@ export default function SemanticViewEditModal({
   show,
   onHide,
   onSave,
-  addDangerToast,
-  addSuccessToast,
+  addDangerToast = () => {},
+  addSuccessToast = () => {},
   semanticView,
 }: SemanticViewEditModalProps) {
   const [description, setDescription] = useState<string>('');
